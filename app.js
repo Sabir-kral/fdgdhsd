5c56e28f9205cb3f377f3b903a4c9f02387fb4ae
Sabir-kral <sabirmemmedli21152014@gmail.com>
2024-12-03 16:48:36 +0400

    superkral

---
+++ app.js
@@ -0,0 +1,16 @@
+function ss() {
+    let ss = document.getElementById("ss").style.backgroundColor = "red"
+    
+}
+function sa() {
+    let ss = document.getElementById("ss").style.backgroundColor = "green"
+    
+}
+function sf() {
+    let ss = document.getElementById("ss").style.backgroundColor = "blue"
+    
+}
+function sd() {
+    let ss = document.getElementById("ss").style.backgroundColor = "purple"
+    
+}
